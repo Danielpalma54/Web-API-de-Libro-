c8a87afc9ebd865d4a623b7895a3c808a779e151
Danielpalma54 <147771801+Danielpalma54@users.noreply.github.com>
2024-06-03 14:36:46 -0500

    Add files via upload

---
+++ src/main/java/com/demo/libros/repositories/LibroRepositoryImpl.java
@@ -0,0 +1,43 @@
+package com.demo.libros.repositories;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.stereotype.Repository;
+import com.demo.libros.models.Libro;
+
+@Repository
+public class LibroRepositoryImpl implements LibroRepository {
+
+    private List<Libro> libros = new ArrayList<>();
+
+    public LibroRepositoryImpl() {
+  
+            libros.add(new Libro(1, "Luna de Pluton", "Dross", "Ediciones Temas de Hoy", 2015));
+            libros.add(new Libro(2, "Orgullo y perjuicio", "Jane Auste", "Austral Editorial", 1915));
+            libros.add(new Libro(3, "Guerra y paz", "Leon Tolstoi", "Ruski Vienik", 1865));
+            libros.add(new Libro(4, "Dracula", "Bram Stoker", "Austral", 1897));
+            libros.add(new Libro(5, "Joyland", "Stephen King ", " DEBOLSILLO,", 2013));
+        }
+    @Override
+    public Libro getLibro(int id) {
+        System.out.println("Conectado a base de datos ORACLE");
+        for (Libro libro : libros) {
+            if (id == libro.getId()) {
+                return libro;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public List<Libro> getLibros() {
+        System.out.println("Conectado a base de datos ORACLE");
+        return libros;
+    }
+
+    @Override
+    public Libro nuevoLibro(Libro libro) {
+        libros.add(libro);
+        return libro;
+    }
+}
